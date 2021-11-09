b2af3043c6b23c5bc5f94b4f441b0930a4f2f4b3
Thom Ritterfeld <thom@ritterfeld.com>
2021-11-09 19:25:42 +0100

    functional component

--- src/WindowsModal.tsx
+++ src/WindowsModal.tsx
@@ -1,35 +1,28 @@
 import * as React from "react";
-import { Component } from "react";
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, ViewProps } from "react-native";
 
-interface Props {
-    visible: boolean;
-    children: object;
-  }
-
-class WindowsModal extends Component<Props, {}> {
-  render () {
-    if (!this.props.visible) return (<View />)
-
-    return (
-      <View style={styles.modal}>
-        {this.props.children}
-      </View>
-    )
-  }
+interface Props extends ViewProps {
+  visible: boolean;
+  children: any;
 }
+
+const WindowsModal: React.FC<Props> = (props) => {
+  if (!props.visible) return <View />;
+  return <View style={styles.modal}>{props.children}</View>;
+};
+
 const styles = StyleSheet.create({
-    modal: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }
+  modal: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default WindowsModal;
